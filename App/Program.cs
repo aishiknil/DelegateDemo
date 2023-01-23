9a79009560c1a85a0490e7268a213cb438217f9b
Aishik Bera <Aishik.Bera@clinisys.com>
2023-01-23 17:47:12 +0530

    Demo1Delegate

---
+++ App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace App
+{
+    delegate int MyDel(int x, int y);
+    public class Program
+
+    {
+        public static int Add(int x, int y)
+        {
+            return (x + y);
+        }
+        public static int Mult(int x, int y)
+        {
+            return (x * y);
+        }
+        static void Main(string[] args)
+        {
+            MyDel del = new MyDel(Add);
+
+        }
+    }
+}
